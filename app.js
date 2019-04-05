3a281d281272428aa279157bb1be1164441f3232
sasirat <sasirat019@gmail.com>
2019-04-05 17:22:51 +0700

    split html

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     console.log(req.url);
-    console.log(req.url.split('/'));
+    // console.log(req.url.split('/'));
     if(req.url === '/'){
         fs.readFile('./public/index.html',(err,content) => {
+            if(err) throw err;
+            res.writeHead(200,{'content-type': 'text/html'});
+            res.end(content);
+        });
+    }else if(req.url === '/gallery'){
+        fs.readFile('./public/gallery.html',(err,content) => {
+            if(err) throw err;
+            res.writeHead(200,{'content-type': 'text/html'});
+            res.end(content);
+        });
+    }else if(req.url === '/contact'){
+        fs.readFile('./public/contact.html',(err,content) => {
             if(err) throw err;
             res.writeHead(200,{'content-type': 'text/html'});
             res.end(content);
